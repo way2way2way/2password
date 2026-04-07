11f1e90a410384f086650b5c502af82c03e1c0d1
way to way to way <238801385+way2way2way@users.noreply.github.com>
2026-04-07 10:48:37 +0900

    Implement hashed password and 2password login
    Refactor password handling to use hashed passwords and secure input.

--- 2password.py
+++ 2password.py
@@ -1,11 +1,28 @@
 #keep is simple stupid!!
+import time
+import sys
+import getpass
+import hashlib
+super = 5
 username = input('set username:')
-password = input('set password:')
-password2 = input('set 2password:')
-loginusername = input('enter username:')
-logintry = input('enter password:')
-logintry2 = input('enter 2password:')
-if password == logintry and password2 == logintry2 and username == loginusername:
-    print('login successful!')
-else:
-    print('error')
+password = getpass.getpass('set password:')
+hash_object = hashlib.sha256(password.encode())
+password = hash_object.hexdigest()
+password2 = getpass.getpass('set 2password:')
+hash_object = hashlib.sha256(password2.encode())
+password2 = hash_object.hexdigest()
+while True:
+    loginusername = input('enter username:')
+    logintry = getpass.getpass('enter password:')
+    hash_object = hashlib.sha256( logintry.encode())
+    logintry = hash_object.hexdigest()
+    logintry2 = getpass.getpass('enter 2password:')
+    hash_object = hashlib.sha256( logintry2.encode())
+    logintry2 = hash_object.hexdigest()
+    if password == logintry and password2 == logintry2 and username == loginusername:
+        print('login successful!')
+        sys.exit()
+    else:
+        time.sleep(super)
+        super += super
+        print('error username or password or 2password miss')
